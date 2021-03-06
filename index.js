4baa30c151e4bf2bcc6c07e2592491accf53565d
Simon <simon.garny@gmail.com>
2021-03-06 09:38:28 +0100

    added fetch and removed sql

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const mysql_connection = require("./mysql_connection");
-
+const mysql = require('mysql');
 
 const client = new Discord.Client();
-const sqlconnection = new mysql_connection(config);
+const con = mysql.createConnection({
+    host: config.DATABASE_HOST,
+    user: config.DATABASE_USER,
+    password: config.DATABASE_PASSWORD,
+    database: config.DATABASE_BASE
+});
+
+con.connect(async function(err) {
+    if (err) {
+        await console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    await console.log('connected as id ' + con.threadId);
+});
 
 const prefix = "!!";
 
@@ -19,24 +32,36 @@ client.on("message", function(message) {
     if (command === "ping") {
         message.reply(`Pong!`);
     } else if (command === "pseudo") {
-        const pseudo = args[0];
-        if (!pseudo) {
+        const metamob_pseudo = args[0];
+        if (!metamob_pseudo) {
             message.reply(`Vous devez fournir un pseudo valide.`);
         }
-        let setResult = sqlconnection.setPseudo(message.member.id, pseudo);
-        if (setResult) {
-            message.reply(`Merci ! Ton pseudo ${pseudo} a bien été enregistré.`);
-        }
+        const discord_id = message.member.id;
+        const discord_pseudo = message.member.displayName;
+        //insert or update
+        con.query("INSERT INTO users(`discord_id`, `discord_name`, `metamob_pseudo`) VALUES (?, ?, ?)" +
+            "ON DUPLICATE KEY UPDATE `discord_id` = ?, `discord_name` = ?, `metamob_pseudo` = ?;",
+            [discord_id, discord_pseudo, metamob_pseudo, discord_id, discord_pseudo, metamob_pseudo], function (error, results, fields) {
+            if (error) {
+                console.error('Erreur d\'insertion...');
+                return;
+            }
+            console.log(`Utilisateur ${metamob_pseudo} a ete enregistré par ${discord_id} (${discord_pseudo})`);
+            message.reply(`Merci ! Ton pseudo ${metamob_pseudo} a bien été enregistré.`);
+        });
+
     } else if (command === "page") {
-        let setResult = sqlconnection.getPseudo(message.member.id);
-        for (var i in setResult) {
-            console.log('oui !');
-            console.log(setResult[i][metamob_pseudo]);
-        }
-        if (setResult) {
-            let metamob_pseudo = setResult.metamob_pseudo;
-            message.reply(`J'ai trouvé ton pseudo Metamob: ${metamob_pseudo} !`);
-        }
+        con.query("SELECT * FROM users WHERE discord_id = ?;",
+            [message.member.id], function (error, results, fields) {
+                if (error) {
+                    console.error('Utilisateur introuvable.');
+                    return;
+                }
+                const resultat = results[0]['metamob_pseudo'];
+                //api call
+                const api_call_result = fetch();
+                message.reply(`Ton pseudo est ${resultat}.`);
+            });
     }
 
 });
