d7b59c164f2b5994fec850e4d290a73e18c38da9
Simon Grn <simon.garny@gmail.com>
2021-03-08 13:49:35 +0100

    added request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const mysql = require('mysql');
+const fetch = require('node-fetch');
 
 const client = new Discord.Client();
 const con = mysql.createConnection({
@@ -21,7 +22,7 @@ con.connect(async function(err) {
 
 const prefix = "!!";
 
-client.on("message", function(message) {
+client.on("message", async function(message) {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
@@ -30,37 +31,45 @@ client.on("message", function(message) {
     const command = args.shift().toLowerCase();
 
     if (command === "ping") {
-        message.reply(`Pong!`);
+        await message.reply(`Pong!`);
     } else if (command === "pseudo") {
         const metamob_pseudo = args[0];
         if (!metamob_pseudo) {
-            message.reply(`Vous devez fournir un pseudo valide.`);
+            await message.reply(`Vous devez fournir un pseudo valide.`);
         }
         const discord_id = message.member.id;
         const discord_pseudo = message.member.displayName;
         //insert or update
         con.query("INSERT INTO users(`discord_id`, `discord_name`, `metamob_pseudo`) VALUES (?, ?, ?)" +
-            "ON DUPLICATE KEY UPDATE `discord_id` = ?, `discord_name` = ?, `metamob_pseudo` = ?;",
-            [discord_id, discord_pseudo, metamob_pseudo, discord_id, discord_pseudo, metamob_pseudo], function (error, results, fields) {
+            "ON DUPLICATE KEY UPDATE `discord_id` = ?, `discord_name` = ?, `metamob_pseudo` = ?, `updated_at` = NOW();",
+            [discord_id, discord_pseudo, metamob_pseudo, discord_id, discord_pseudo, metamob_pseudo],
+            async function (error, results, fields) {
             if (error) {
-                console.error('Erreur d\'insertion...');
+                await message.reply(`Il y a eu une erreur lors de l'enregistrement de ton pseudo Metamob.`);
+                console.error(`Erreur d'insertion pour le `);
                 return;
             }
             console.log(`Utilisateur ${metamob_pseudo} a ete enregistré par ${discord_id} (${discord_pseudo})`);
-            message.reply(`Merci ! Ton pseudo ${metamob_pseudo} a bien été enregistré.`);
+                await message.reply(`Merci ! Ton pseudo ${metamob_pseudo} a bien été enregistré.`);
         });
 
     } else if (command === "page") {
-        con.query("SELECT * FROM users WHERE discord_id = ?;",
-            [message.member.id], function (error, results, fields) {
+        await con.query("SELECT * FROM users WHERE discord_id = ?;",
+            [message.member.id], async function (error, results, fields) {
                 if (error) {
                     console.error('Utilisateur introuvable.');
                     return;
                 }
-                const resultat = results[0]['metamob_pseudo'];
+                const metamob_pseudo = results[0]['metamob_pseudo'];
                 //api call
-                const api_call_result = fetch();
-                message.reply(`Ton pseudo est ${resultat}.`);
+                let fetch_result = fetch(config.METAMOB_API_BASE_URL+'utilisateurs/'+metamob_pseudo,
+                    {
+                    headers: {'Content-Type': 'application/json', 'HTTP-X-APIKEY': config.METAMOB_API_KEY},
+                })
+                    .then(res => res.json())
+                    .then(resolve(fetch_result));
+                await console.log(fetch_result);
+                // await message.reply(`Ton pseudo est ${metamob_pseudo}.`);
             });
     }
 
